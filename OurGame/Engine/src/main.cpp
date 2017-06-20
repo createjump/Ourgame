ac695b88ee46b61862ada2af95ead68e8e3dd4b1
createjump <joeypence88@gmail.com>
2017-06-19 23:06:05 -0400

    Add VS2017 source

---
+++ OurGame/Engine/src/main.cpp
@@ -0,0 +1,19 @@
+/*				*\
+	Ourgame .01
+==================
+Licensed under GPLv2.
+
+\*				*/
+
+// File: main.cpp
+//
+// Purpose: Main entry point for program.
+//
+
+#include "pubinc\common.h"
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	cout << "Hello Engine World!\n";
+	return 0;
+}
---
+++ OurGame/Engine/src/pubinc/common.h
@@ -0,0 +1,17 @@
+#pragma once
+/*				*\
+Ourgame .01
+==================
+Licensed under GPLv2.
+
+\*				*/
+
+// File: common.h
+//
+// Purpose: Common includes.
+//
+
+
+#include <iostream>
+#include <stdio.h>
+#include <ostream>
